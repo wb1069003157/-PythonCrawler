057d37e9e3759e7ee9ca7bb3f72fd6b8c4e5ee70
Bin <1069003157@qq.com>
2017-09-24 22:40:38 +0800

    Initial completion procedure

---
+++ baike_spider/html_downloader.py
@@ -0,0 +1,11 @@
+# coding:utf-8
+from urllib import request
+
+class HtmlDownloader(object):
+    def downloader(self, url):
+        if url is None:
+            return None
+        response=request.urlopen(url)
+        if response.getcode() !=200:
+            return None
+        return response.read()
